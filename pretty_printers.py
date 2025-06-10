70f588e23f28b74d8ebb0b6d440ca7a15f94c412
Mathiscrack <mathisaac03@gmail.com>
2025-06-10 12:44:38 +0200

    pp struct use

--- pretty_printers.py
+++ pretty_printers.py
@@ -33,8 +33,11 @@ def pp_commande(c, indent=0):
         decla = c.children[0]
         exp = c.children[1]
         return f"{tab}{pp_declaration(decla)} = {pp_expression(exp)};"
-    if "struct" in c.data:
-        return pp_struct_sanstypedef(c, indent)
+    if "struct_attr_affect" in c.data:
+        struct_name = c.children[0]
+        attr_name = c.children[1]
+        exp = c.children[2]
+        return f"{tab}{struct_name}.{attr_name} = {pp_expression(exp)}"
     if c.data == "skip":
         return f"{tab}skip;"
     if c.data == "print":
@@ -51,28 +54,6 @@ def pp_commande(c, indent=0):
             return f"{tab}if ({pp_expression(exp)}) {{\n{pp_bloc(com, indent + 1)}{tab}}} else {{\n{pp_bloc(com_else, indent + 1)}{tab}}}"
         return f"{tab}if ({pp_expression(exp)}) {{\n{pp_bloc(com, indent + 1)}{tab}}}"
 
-def pp_struct_sanstypedef(s, indent=0):
-    tab = "    " * indent
-    if s.data == "structs_def":
-        name = s.children[0].value
-        decls = s.children[1:]
-        str_declarations = ""
-        for decl in decls[:-1]:
-            str_declarations += 2*tab + pp_declaration(decl) + ";\n"
-        str_declarations += 2*tab + pp_declaration(decls[-1]) + ";"
-        return f"{tab}struct {name} {{\n{str_declarations}\n{tab}}};"
-    if s.data == "struct_init_seq":
-        struct_name = s.children[0].value
-        entity_name = s.children[1].value
-        if len(s.children) == 2:
-            return f"{tab}struct {struct_name} {entity_name};"
-        else:
-            exps = s.children[2:]
-            str_expressions = ""
-            for exp in exps[:-1]:
-                str_expressions += pp_expression(exp) + ", "
-            str_expressions += pp_expression(exps[-1])
-            return f"{tab}struct {struct_name} {entity_name} {{{str_expressions}}};"
 
 def pp_struct_typedef(s):
     tab = "    "
