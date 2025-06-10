1ab93dc513868c97427c9ad1011a313e6f4fc711
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-06-10 14:38:33 +0200

    feat : add pretty printer.

--- pretty_printers.py
+++ pretty_printers.py
@@ -9,11 +9,12 @@ def pp_expression(e):
     if e.data == "cast_double":
         exp = e.children[0]
         return f"(double)({pp_expression(exp)})"
-    # &var
     if e.data == "addr_of":
         var = e.children[0]
         return f"&{var.value}"
-    # malloc()
+    if e.data == "dereference":
+        var = e.children[0]
+        return f"*{var.value}"
     if e.data == "malloc_call":
         return "malloc()"
     if e.data == "struct_attr_use":
