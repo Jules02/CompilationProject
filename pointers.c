44cf2e96cebdc38f9b2d388c52c7e593c9c83348
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-06-10 13:35:15 +0200

    feat : add struct pointer.

--- pointers.c
+++ pointers.c
@@ -1,16 +1,25 @@
+typedef struct
+{
+    long num;
+} Test;
+
 long main()
 {
     long num;
     long ptrValHolder;
 
     num = 12345;
+    printf(num);
+
     long *ptrNum;
     ptrNum = &num;
 
-    printf(num);
-
     ptrValHolder = *ptrNum;
     printf(ptrValHolder);
+
+    Test *test;
+    test.num = 4;
+    printf(test.num);
 
     return (0);
 }
